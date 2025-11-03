9f80e5d3e73b1154db42ad5d575047c4c89452f7
yolln z <yollnz1217@gmail.com>
2025-11-03 13:52:24 +0000

    todo syscall and pause

--- Project-3/myCPU/WB.v
+++ Project-3/myCPU/WB.v
@@ -2,12 +2,22 @@ module WB(
         input   wire            clk,
         input   wire            rst,
         input   wire [102:0]    MEM_to_WB_zip,
+        input   wire [ 81:0]    MEM_except_reg,
         
         output  wire            WB_allowin,
         output  wire            rf_wen,
         output  wire [  4:0]    rf_waddr,
-        output  wire [ 31:0]    rf_wdata,
-        output  reg  [ 72:0]    inst_retire_reg
+        output  wire [ 31:0]    rf_wdata_final,
+        output  reg  [ 72:0]    inst_retire_reg,
+
+
+        output  wire            csr_re,
+        output  wire [13:0]     csr_num,
+        input   wire [31:0]     csr_rvalue,
+        output  wire            csr_we,
+        output  wire [31:0]     csr_wmask,
+        output  wire [31:0]     csr_wvalue,
+        output  wire            ertn_flush
 );
 
 wire            valid;
@@ -21,10 +31,12 @@ assign {
     valid, pc, IR, gr_we, rf_waddr, rf_wdata
 } = MEM_to_WB_zip;
 
-assign rf_wen   = gr_we & valid;
+assign {csr_re, csr_we, csr_wmask, csr_wvalue, csr_num, ertn_flush, inst_syscall} = MEM_except_reg;
 
+assign rf_wen   = gr_we & valid;
+assign rf_wdata_final = csr_re ? csr_rvalue : rf_wdata;
 always @(posedge clk) begin
-        inst_retire_reg <= {pc, {4{rf_wen}}, rf_waddr, rf_wdata};
+        inst_retire_reg <= {pc, {4{rf_wen}}, rf_waddr, rf_wdata_final};
 end
 
 endmodule
