59a847d53d9c50c12b37c3ab781ca88a30aef229
qianhanlin23 <qianhanlin23@mails.ucas.ac.cn>
2025-11-03 02:05:47 +0000

    init csr.v

--- Project-3/myCPU/csr.v
+++ Project-3/myCPU/csr.v
@@ -1,7 +1,67 @@
+`define CSR_CRMD    14'h00
+`define CSR_PRMD    14'h01
+`define CSR_EUEN    14'h02
+`define CSR_ECFG    14'h04
+`define CSR_ESTAT   14'h05
+`define CSR_ERA     14'h06
+`define CSR_BADV    14'h07
+`define CSR_EENTRY  14'h0c
+`define CSR_SAVE0   14'h30
+`define CSR_SAVE1   14'h31
+`define CSR_SAVE2   14'h32
+`define CSR_SAVE3   14'h33
+`define CSR_TICLR   14'h44
+
+
+`define CSR_CRMD_PLV    1:0
+`define CSR_CRMD_PIE    2
+
+`define CSR_PRMD_PPLV   1:0
+`define CSR_PRMD_PIE    2
+
+`define CSR_ESTAT_IS10  1:0
+`define CSR_ERA_PC      31:0
+`define CSR_EENTRY_VA   31:6
+`define CSR_SAVE_DATA   31:0
+
+
 module csr(
-        input   
-        output 
+        input   wire            clk,
+        input   wire            reset,
+        input   wire            csr_re,
+        input   wire [13:0]     csr_num,
+        output  wire [31:0]     csr_rvalue,
+        input   wire            csr_we,
+        input   wire [31:0]     csr_wmask,
+        input   wire [31:0]     csr_wvalue      
 );
+
+wire [31: 0] csr_crmd_data;
+reg  [ 1: 0] csr_crmd_plv;      
+reg          csr_crmd_ie;       
+wire         csr_crmd_da;       
+wire         csr_crmd_pg;
+wire [ 6: 5] csr_crmd_datf;
+wire [ 8: 7] csr_crmd_datm;
+
+wire [31: 0] csr_prmd_data;
+reg  [ 1: 0] csr_prmd_pplv;     
+reg          csr_prmd_pie; 
+
+wire [31: 0] csr_estat_data;    
+reg  [12: 0] csr_estat_is;      
+reg  [ 5: 0] csr_estat_ecode;   
+reg  [ 8: 0] csr_estat_esubcode;
+
+reg  [31: 0] csr_era_pc;  
+wire [31: 0] csr_eentry_data;   
+reg  [25: 0] csr_eentry_va;  
+
+reg  [31: 0] csr_save0_data;
+reg  [31: 0] csr_save1_data;
+reg  [31: 0] csr_save2_data;
+reg  [31: 0] csr_save3_data;
+
 
 // CRMD
 always @(posedge clock) begin
@@ -32,6 +92,8 @@ assign csr_crmd_da   = 1'b1;
 assign csr_crmd_pg   = 1'b0; 
 assign csr_crmd_datf = 2'b00; 
 assign csr_crmd_datm = 2'b00;
+assign csr_crmd_data = {23'b0, csr_crmd_datm, csr_crmd_datf, csr_crmd_pg, 
+                            csr_crmd_da, csr_crmd_ie, csr_crmd_plv};
 
 // PRMD
 always @(posedge clock) begin
@@ -46,6 +108,7 @@ always @(posedge clock) begin
                       | ~csr_wmask[`CSR_PRMD_PIE]&csr_prmd_pie;
     end
  end
+assign csr_prmd_data  = {29'b0, csr_prmd_pie, csr_prmd_pplv};
 
 // ESTAT
 always @(posedge clock) begin
@@ -70,6 +133,7 @@ always @(posedge clock) begin
         csr_estat_esubcode <= wb_esubcode;
     end
  end
+assign csr_estat_data = { 1'b0, csr_estat_esubcode, csr_estat_ecode, 3'b0, csr_estat_is};
 
 // ERA
 always @(posedge clock) begin
@@ -86,6 +150,7 @@ always @(posedge clock) begin
         csr_eentry_va <= csr_wmask[`CSR_EENTRY_VA]&csr_wvalue[`CSR_EENTRY_VA]
                        | ~csr_wmask[`CSR_EENTRY_VA]&csr_eentry_va;
  end
+assign csr_eentry_data= {csr_eentry_va, 6'b0};
 
 // SAVE0～3
 always @(posedge clock) begin
@@ -102,5 +167,5 @@ always @(posedge clock) begin
         csr_save3_data <= csr_wmask[`CSR_SAVE_DATA]&csr_wvalue[`CSR_SAVE_DATA]
                        | ~csr_wmask[`CSR_SAVE_DATA]&csr_save3_data;
  end
- 
+
 endmodule
