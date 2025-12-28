299cc6b025dce15c04159465c11addc2fd5d4c59
yolln z <yollnz1217@gmail.com>
2025-12-28 13:59:14 +0000

    burst before debug

--- Project-7/myCPU/bridge.v
+++ Project-7/myCPU/bridge.v
@@ -2,31 +2,36 @@ module bridge(
     input           aclk,
     input           aresetn,
 
-    input           inst_sram_req,
-    input           inst_sram_wr,
-    input  [1:0]    inst_sram_size,
-    input  [31:0]   inst_sram_addr,
-    input  [3:0]    inst_sram_wstrb,
-    input  [31:0]   inst_sram_wdata,
-    output [31:0]   inst_sram_rdata,
-    output          inst_sram_addr_ok,
-    output          inst_sram_data_ok,
+    // ICache 读接口
+    input               icache_rd_req,
+    input   [2:0]       icache_rd_type,
+    input   [31:0]      icache_rd_addr,
+    output              icache_rd_rdy,
+    output              icache_ret_valid,
+    output              icache_ret_last,
+    output  [31:0]      icache_ret_data,
+    
+    // DCache 读接口
+    input               dcache_rd_req,
+    input   [2:0]       dcache_rd_type,
+    input   [31:0]      dcache_rd_addr,
+    output              dcache_rd_rdy,
+    output              dcache_ret_valid,
+    output              dcache_ret_last,
+    output  [31:0]      dcache_ret_data,
+    
+    // DCache 写接口
+    input               dcache_wr_req,
+    input   [2:0]       dcache_wr_type,
+    input   [31:0]      dcache_wr_addr,
+    input   [3:0]       dcache_wr_wstrb,
+    input   [127:0]     dcache_wr_data,
+    output              dcache_wr_rdy,
 
-    input           data_sram_req,
-    input           data_sram_wr,
-    input  [1:0]    data_sram_size,
-    input  [31:0]   data_sram_addr,
-    input  [3:0]    data_sram_wstrb,
-    input  [31:0]   data_sram_wdata,
-    output [31:0]   data_sram_rdata,
-    output          data_sram_addr_ok,
-    output          data_sram_data_ok,
-
-
-    // ar    读请求通道
+    // ar 读请求通道
     output reg[3:0]    arid,
     output reg[31:0]   araddr,
-    output    [7:0]    arlen,
+    output reg[7:0]    arlen,
     output reg[2:0]    arsize,    
     output    [1:0]    arburst,
     output    [1:0]    arlock,
@@ -34,7 +39,8 @@ module bridge(
     output    [2:0]    arprot,
     output             arvalid, 
     input              arready,
-    // r  读响应通道
+    
+    // r 读响应通道
     input  [3:0]       rid,
     input  [31:0]      rdata,
     input  [1:0]       rresp,
@@ -42,10 +48,10 @@ module bridge(
     input              rvalid,
     output             rready,
 
-    // aw  写请求通道
+    // aw 写请求通道
     output    [3:0]    awid,
     output reg[31:0]   awaddr,
-    output    [7:0]    awlen,
+    output reg[7:0]    awlen,
     output reg[2:0]    awsize,
     output    [1:0]    awburst,
     output    [1:0]    awlock,
@@ -54,7 +60,7 @@ module bridge(
     output             awvalid,
     input              awready,
 
-    // w  写数据通道
+    // w 写数据通道
     output    [3:0]    wid,
     output reg[31:0]   wdata,
     output reg[3:0]    wstrb,
@@ -62,50 +68,45 @@ module bridge(
     output             wvalid,
     input              wready,
 
-    // b  写响应通道
+    // b 写响应通道
     input  [3:0]       bid,
     input  [1:0]       bresp,
     input              bvalid,
     output             bready
 );
 
+// Cache 请求信号
 wire rinst_req;
-wire winst_req;
 wire rdata_req;
 wire wdata_req;
 
-// 读写请求
-assign rinst_req = inst_sram_req & ~inst_sram_wr;      
-assign winst_req = inst_sram_req & inst_sram_wr;
-assign rdata_req = data_sram_req & ~data_sram_wr;
-assign wdata_req = data_sram_req & data_sram_wr;
+assign rinst_req = icache_rd_req;
+assign rdata_req = dcache_rd_req;
+assign wdata_req = dcache_wr_req;
 
-
-            //读请求状态机
+// 状态机常量定义
 localparam  ar_wait         = 2'b01,
             ar_req          = 2'b10,
 
-            //读响应状态机
-            r_wait          = 3'b001,
-            r_read          = 3'b010,
-            r_done          = 3'b100,
+            r_wait          = 4'b0001,
+            r_start         = 4'b0010,  
+            r_reading       = 4'b0100, 
+            r_done          = 4'b1000,
 
-            //写请求、数据状态机
             w_wait          = 5'b00001,
             w_wait_aw_w     = 5'b00010,
             w_wait_w        = 5'b00100,
             w_wait_aw       = 5'b01000,
             w_done          = 5'b10000,
 
-            //写响应状态机
             b_wait          = 2'b01,
             b_done          = 2'b10;
 
 reg [1:0] ar_cur_state;
 reg [1:0] ar_next_state;
 
-reg [2:0] r_cur_state;
-reg [2:0] r_next_state;
+reg [3:0] r_cur_state;
+reg [3:0] r_next_state;
 
 reg [4:0] w_cur_state;
 reg [4:0] w_next_state;
@@ -115,10 +116,12 @@ reg [1:0] b_next_state;
 
 reg [1:0]  r_cnt;
 reg [3:0]  rid_reg;
-reg [31:0] inst_rdata_reg;
-reg [31:0] data_rdata_reg;
 reg [31:0] rdata_buffer [1:0];
-// reg [3:0]  rid_buffer   [3:0];
+
+// 写 Burst 相关寄存器
+reg [1:0]   wdata_index;
+reg [127:0] dcache_wr_data_r;
+reg [3:0]   dcache_wr_wstrb_r;
 
 wire reset;
 assign reset = ~aresetn;
@@ -127,29 +130,30 @@ wire need_wait;
 assign need_wait = (araddr == awaddr) & (|w_cur_state[4:1]);
 
 // read_buffer
-always @(posedge aclk)begin
-    if(reset)begin
+always @(posedge aclk) begin
+    if(reset) begin
         rdata_buffer[0] <= 32'b0;
         rdata_buffer[1] <= 32'b0;
     end
-    else if(rready & rvalid)begin
+    else if(rready & rvalid) begin
         rdata_buffer[rid[0]] <= rdata;
     end
 end
 
- always @(posedge aclk) begin
-        if(reset)
-            r_cnt <= 2'b0;
-        else if(arvalid & arready & rvalid & rready)
-            r_cnt <= r_cnt;
-        else if(arvalid & arready)
-            r_cnt <= r_cnt + 2'b1;
-        else if(rvalid & rready)
-            r_cnt <= r_cnt - 2'b1;
-    end
+// r_cnt
+always @(posedge aclk) begin
+    if(reset)
+        r_cnt <= 2'b0;
+    else if(arvalid & arready & rvalid & rready)
+        r_cnt <= r_cnt;
+    else if(arvalid & arready)
+        r_cnt <= r_cnt + 2'b1;
+    else if(rvalid & rready)
+        r_cnt <= r_cnt - 2'b1;
+end
 
-// 读请求状态机
-always @(posedge aclk)begin
+// 读请求状态机 
+always @(posedge aclk) begin
     if(reset)
         ar_cur_state <= ar_wait;
     else
@@ -177,7 +181,8 @@ always @(*) begin
     endcase
 end
 
-// 读响应状态机
+
+// 读响应状态机 (R Channel) - 新增reading状态以支持 Burst
 always @(posedge aclk) begin
     if(reset)
         r_cur_state <= r_wait;
@@ -189,73 +194,102 @@ always @(*) begin
     case(r_cur_state)
         r_wait: begin
             if(arvalid & arready | (|r_cnt))
-                r_next_state = r_read;
+                r_next_state = r_start;
             else
                 r_next_state = r_wait;
         end
-        r_read: begin
-            if(rvalid & rready)
-                r_next_state = r_done;
+        r_start: begin
+            if(rvalid & rready) begin
+                if(rlast)
+                    r_next_state = r_done;
+                else
+                    r_next_state = r_reading;
+            end
             else
-                r_next_state = r_read;
+                r_next_state = r_start;
+        end
+        r_reading: begin
+            if(rvalid & rready) begin
+                if(rlast)
+                    r_next_state = r_done;
+                else
+                    r_next_state = r_reading;
+            end
+            else
+                r_next_state = r_reading;
         end
         r_done:
-                r_next_state = r_wait;
+            r_next_state = r_wait;
         default:
-                r_next_state = r_wait;
+            r_next_state = r_wait;
     endcase
 end
 
-// 写请求状态机
+
+// 写请求状态机 (AW & W Channel)
 always @(posedge aclk) begin
     if(reset)
-            w_cur_state <= w_wait;
+        w_cur_state <= w_wait;
     else 
-            w_cur_state <= w_next_state;
+        w_cur_state <= w_next_state;
 end
 
 always @(*) begin
     case(w_cur_state)
         w_wait: begin
             if(wdata_req)
-                    w_next_state = w_wait_aw_w;
+                w_next_state = w_wait_aw_w;
             else
-                    w_next_state = w_wait;
+                w_next_state = w_wait;
         end
         w_wait_aw_w: begin
-            if(awvalid & awready & wvalid & wready)
+            // 检查 wlast，支持多拍传输
+            if(awvalid & awready & wvalid & wready) begin
+                if(wlast)  // 地址和最后一拍数据同时握手
                     w_next_state = w_done;
-            else if(awvalid & awready)
+                else       // 地址握手了，但数据还没发完
                     w_next_state = w_wait_w;
-            else if(wvalid & wready)
+            end
+            else if(awvalid & awready)
+                w_next_state = w_wait_w;
+            else if(wvalid & wready) begin
+                if(wlast)  // 最后一拍数据握手了，但地址还没握手
                     w_next_state = w_wait_aw;
+                else       // 数据握手了，但还有后续数据
+                    w_next_state = w_wait_aw_w;  // 继续等待
+            end
             else
-                    w_next_state = w_wait_aw_w;
+                w_next_state = w_wait_aw_w;
         end
         w_wait_aw: begin
             if(awvalid & awready) 
-                    w_next_state = w_done;
+                w_next_state = w_done;
             else 
-                    w_next_state = w_wait_aw;
+                w_next_state = w_wait_aw;
         end
         w_wait_w: begin
-            if(wvalid & wready)
+            // 检查 wlast，支持多拍传输
+            if(wvalid & wready) begin
+                if(wlast)  // 最后一拍数据发送完成
                     w_next_state = w_done;
-            else
+                else       // 还有后续数据，继续发送
                     w_next_state = w_wait_w;
+            end
+            else
+                w_next_state = w_wait_w;
         end
         w_done: begin
             if(bvalid & bready)
-                    w_next_state = w_wait;
+                w_next_state = w_wait;
             else
-                    w_next_state = w_done;
+                w_next_state = w_done;
         end
         default:
-                    w_next_state = w_wait;
+            w_next_state = w_wait;
     endcase
 end
 
-// 写响应状态机
+
 always @(posedge aclk) begin
     if(reset)
         b_cur_state <= b_wait;
@@ -266,7 +300,7 @@ end
 always @(*) begin
     case(b_cur_state)
         b_wait: begin
-            if(wvalid && wready)
+            if(wvalid && wready && wlast)
                 b_next_state = b_done;
             else
                 b_next_state = b_wait;
@@ -278,108 +312,152 @@ always @(*) begin
                 b_next_state = b_done;
         end
         default:
-                b_next_state = b_wait;
+            b_next_state = b_wait;
     endcase
 end
 
 
+// AXI 控制信号
 assign arvalid = ar_cur_state[1];
-assign rready  = r_cur_state[1];
+assign rready  = r_cur_state[1] | r_cur_state[2];  // r_start 或 r_reading
 
 assign awvalid = w_cur_state[1] | w_cur_state[3];
 assign wvalid  = w_cur_state[1] | w_cur_state[2];
 assign bready  = w_cur_state[4];
-
-
-
 
 always @(posedge aclk) begin
     if(reset) begin
         arid <= 4'b0;
     end
     else if(ar_cur_state[0]) begin
-        arid <= {4{rdata_req}}; // 数据RAM请求优先于指令RAM
+        arid <= {3'b0, rdata_req};
     end
 end
-
-
 
 always @(posedge aclk) begin
     if(reset) begin
         araddr <= 32'b0;
     end
     else if(ar_cur_state[0]) begin
-        if (rdata_req)
-            araddr <= data_sram_addr;
-        else
-            araddr <= inst_sram_addr;
+        araddr <= rdata_req ? dcache_rd_addr : icache_rd_addr;
     end
 end
-
 
 always @(posedge aclk) begin
     if(reset) begin
         arsize <= 3'b0;
     end
     else if(ar_cur_state[0]) begin
-        if (rdata_req)
-            arsize <= {1'b0, data_sram_size};
-        else
-            arsize <= {1'b0, inst_sram_size};
+        arsize <= rdata_req ? {1'b0, dcache_rd_type[1:0]} : {1'b0, icache_rd_type[1:0]};
     end
 end
 
-always  @(posedge aclk) begin
-        if(reset) begin
-                awaddr <= 32'b0;
-                awsize <= 3'b0;
-        end
-        else if(w_cur_state[0]) begin	
-                awaddr <= data_sram_wr? data_sram_addr : inst_sram_addr;
-                awsize <= data_sram_wr? {1'b0, data_sram_size} : {1'b0, inst_sram_size};
-        end
+always @(posedge aclk) begin
+    if(reset) begin
+        arlen <= 8'b0;
+    end
+    else if(ar_cur_state[0]) begin
+        arlen[1:0] <= rdata_req ? {2{dcache_rd_type[2]}} : {2{icache_rd_type[2]}};
+    end
 end
 
-always  @(posedge aclk) begin
-        if(reset) begin
-                wstrb <= 4'b0;
-                wdata <= 32'b0;
-        end
-        else if(w_cur_state[0]) begin
-                wstrb <= data_sram_wstrb;
-                wdata <= data_sram_wdata;
-        end
-end
-
-assign arlen   = 8'b0;
 assign arburst = 2'b01;
 assign arlock  = 2'b0;
 assign arcache = 4'b0;
 assign arprot  = 3'b0;
 
-assign awid     = 4'b0001;
-assign awlen    = 8'b0;
-assign awburst  = 2'b01;
-assign awlock   = 2'b0;
-assign awcache  = 4'b0;
-assign awprot   = 3'b0;
-
-assign wid      = 4'b0001;
-assign wlast    = 1'b1;
-
 always @(posedge aclk) begin
-        if(reset)
-            rid_reg <= 4'b0;
-        else if(rvalid & rready)
-            rid_reg <= rid;
+    if(reset) begin
+        awaddr <= 32'b0;
+        awsize <= 3'b0;
+    end
+    else if(w_cur_state[0]) begin
+        awaddr <= dcache_wr_addr;
+        awsize <= {1'b0, dcache_wr_type[1:0]};
+    end
 end
 
-assign inst_sram_rdata = rdata_buffer[0];
-assign data_sram_rdata = rdata_buffer[1];
+always @(posedge aclk) begin
+    if(reset) begin
+        awlen <= 8'b0;
+    end
+    else if(w_cur_state[0]) begin
+        awlen[1:0] <= {2{dcache_wr_type[2]}};
+    end
+end
 
-assign inst_sram_addr_ok = ~arid[0] & arvalid & arready | ~awid[0] & awvalid & awready;
-assign data_sram_addr_ok =  arid[0] & arvalid & arready |  awid[0] & awvalid & awready; 
+assign awid    = 4'b0001;
+assign awburst = 2'b01;
+assign awlock  = 2'b0;
+assign awcache = 4'b0;
+assign awprot  = 3'b0;
 
-assign inst_sram_data_ok = (~rid_reg[0] & r_cur_state[2]);
-assign data_sram_data_ok =  (rid_reg[0] & r_cur_state[2]) | (bvalid & bready);
+
+
+// 写数据缓存
+always @(posedge aclk) begin
+    if(reset) begin
+        dcache_wr_data_r <= 128'b0;
+        dcache_wr_wstrb_r <= 4'b0;
+    end
+    else if(w_cur_state[0]) begin
+        dcache_wr_data_r <= dcache_wr_data;
+        dcache_wr_wstrb_r <= dcache_wr_wstrb;
+    end
+end
+
+// 索引计数器
+always @(posedge aclk) begin
+    if(reset)
+        wdata_index <= 2'b0;
+    else if(w_cur_state[0])
+        wdata_index <= 2'b0;
+    else if(wvalid & wready)
+        wdata_index <= wdata_index + 2'b1;
+end
+
+// 根据索引选择数据
+always @(posedge aclk) begin
+    if(reset) begin
+        wstrb <= 4'b0;
+        wdata <= 32'b0;
+    end
+    else if(w_cur_state[0] | (wvalid & wready)) begin
+        wstrb <= dcache_wr_wstrb_r;
+        case(wdata_index)
+            2'b00: wdata <= dcache_wr_data_r[31:0];
+            2'b01: wdata <= dcache_wr_data_r[63:32];
+            2'b10: wdata <= dcache_wr_data_r[95:64];
+            2'b11: wdata <= dcache_wr_data_r[127:96];
+        endcase
+    end
+end
+
+assign wid   = 4'b0001;
+assign wlast = &wdata_index;
+
+
+always @(posedge aclk) begin
+    if(reset)
+        rid_reg <= 4'b0;
+    else if(rvalid & rready)
+        rid_reg <= rid;
+end
+
+
+// ICache 读接口
+assign icache_rd_rdy     = ar_cur_state[0] & ~rdata_req;
+assign icache_ret_data   = rdata_buffer[0];
+assign icache_ret_valid  = ~rid_reg[0] & (|r_cur_state[3:2]);
+assign icache_ret_last   = ~rid_reg[0] & r_cur_state[3];
+
+// DCache 读接口
+assign dcache_rd_rdy     = ar_cur_state[0] & rdata_req;
+assign dcache_ret_data   = rdata_buffer[1];
+assign dcache_ret_valid  = rid_reg[0] & (|r_cur_state[3:2]);
+assign dcache_ret_last   = rid_reg[0] & r_cur_state[3];
+
+// DCache 写接口
+assign dcache_wr_rdy     = b_cur_state[0];
+
 endmodule
